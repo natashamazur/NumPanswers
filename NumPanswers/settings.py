710a780455353ceb244d609ade756a509d0b5ba7
17824381 <maksimov.i.vl@sberbank.ru>
2020-11-28 03:31:06 +0300

    add remote connect permission

--- NumPanswers/settings.py
+++ NumPanswers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wm8d9^t6ggr!#(m9e=37c$6-kl%gf0ms5uf+9#wnm(nfg$)&&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
