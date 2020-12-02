2cdd323543ef1a576f94e1183fc71f9d8acd29d4
17824381 <maksimov.i.vl@sberbank.ru>
2020-12-03 02:14:51 +0300

    add postgres

--- NumPanswers/settings.py
+++ NumPanswers/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'NumPanswers.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'articles',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
